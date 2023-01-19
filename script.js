592aa35a339bb4af7a243b2178e1553d66d5b886
Shlok kumar <58381931+shlk7@users.noreply.github.com>
2023-01-19 10:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+	String temp=str.toLowerCase();
+	var start=0;
+	var end=str.length()-1;
+	while(start<end){
+		if(temp.charAt(start)!=temp.charAt(end)) return false;
+		start++;
+		end--;
+	}
+	return true;
 
 }
 module.exports = palindrome
